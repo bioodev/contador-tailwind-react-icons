a24d90a3b1d2dd7e60467a4f359faa87779e1b49
bioodev <bioodev@protonmail.com>
2022-11-11 19:37:57 -0300

    .

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,36 @@
 import { useState } from "react";
+import { FcExpand, FcCollapse, FcSynchronize } from "react-icons/fc";
 import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0);
+  const [contador, setContador] = useState(0);
+  let estiloBoton =
+    "bg-white text-black font-bold text-3xl shadow m-2 px-8 py-4 rounded-lg";
 
   return (
-    <div className="App">
-      <div className="borrame-desde-aqui">
-        <h1 className="text-3xl font-bold underline p-5">Hola mundo</h1>
-        <h2 className="text-1xl">
-          Plantilla para iniciar proyectos. Utilizando Vite & Tailwindcss
-        </h2>
-        <div className="card">
-          <button onClick={() => setCount((count) => count + 1)}>
-            Cuenta en {count}
-          </button>
-        </div>
+    <div className="App w-screen h-screen flex flex-col items-center justify-center text-center">
+      <div className="container">
+        <h1 className="text-black text-9xl text-center leading-none">{contador}</h1>
+      </div>
+      <div className="container">
+        <button
+          className={estiloBoton}
+          onClick={() => setContador((contador) => contador - 1)}
+        >
+          <FcExpand />
+        </button>
+        <button
+          className={estiloBoton}
+          onClick={() => setContador((contador) => (contador = 0))}
+        >
+          <FcSynchronize />
+        </button>
+        <button
+          className={estiloBoton}
+          onClick={() => setContador((contador) => contador + 1)}
+        >
+          <FcCollapse />
+        </button>
       </div>
     </div>
   );
