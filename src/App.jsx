a65702e3953f9ad5ca8fae2b43805a5f5335d871
bioodev <bioodev@protonmail.com>
2022-11-12 00:31:05 -0300

    ..

--- src/App.jsx
+++ src/App.jsx
@@ -5,12 +5,12 @@ import "./App.css";
 function App() {
   const [contador, setContador] = useState(0);
   let estiloBoton =
-    "bg-white text-black font-bold text-3xl shadow m-2 px-8 py-4 rounded-lg";
+    "bg-white hover:shadow-md hover:bg-yellow-100 hover:scale-125 active:bg-red-300 active:shadow-xl active:scale-150 duration-300 text-black font-bold text-3xl shadow m-2 px-8 py-4 rounded-lg";
 
   return (
     <div className="App w-screen h-screen flex flex-col items-center justify-center text-center">
       <div className="container">
-        <h1 className="text-black text-9xl text-center leading-none">{contador}</h1>
+        <h1 className="text-black text-9xl text-center leading-none duration-500 ">{contador}</h1>
       </div>
       <div className="container">
         <button
